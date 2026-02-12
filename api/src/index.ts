916b067ffb922c6c613a52fa70a453a02fa29d17
IMatheusmaia <i.matheuslmaia@gmail.com>
2026-02-12 01:43:44 -0300

    breakpoint-02: excluido componentes criados pelo agent

--- api/src/index.ts
+++ api/src/index.ts
@@ -10,7 +10,7 @@ const fastify = Fastify({
 });
 
 await fastify.register(cors, {
-    origin: ['http://localhost:4321'],
+    origin: ['http://localhost:4322'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
